cbe728255c88009f628456c2afb96345fea56b08
binglan00 <452950672@qq.com>
2019-08-17 21:13:39 +0800

    123

--- src/main/java/com/chen/tmall/config/WebMvcConfigurer.java
+++ src/main/java/com/chen/tmall/config/WebMvcConfigurer.java
@@ -13,10 +13,13 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         return new LoginInterceptor();
     }
 
+
     @Bean
     public OtherInterceptor getOtherIntercepter() {
         return new OtherInterceptor();
     }
+
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
